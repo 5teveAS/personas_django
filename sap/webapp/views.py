d3213d4236c08c6c8c6402a553430a4fd10ccc2a
5teveAS <stevarcsa@gmail.com>
2024-07-05 20:58:16 -0600

    personas_django

---
+++ sap/webapp/views.py
@@ -0,0 +1,15 @@
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+
+# def bienvenido(request):
+#     return HttpResponse("Hola mundo")
+
+def bienvenido(request):
+    return render(request, 'bienvenido.html')
+
+def despedirse(request):
+    return HttpResponse("despedida desde django")
